63954acd291609298283308ee7a166e8dae1584f
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-05-03 08:22:33 -0500

    feat(src/components/Footer-Header-Profile-Sidebar-Stories) add content

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+  <div class="Profile-content">
+    <div class="Profile-avatar">
+      <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+    </div>
+    <div class="Profile-info">
+      <h2>nextwo</h2>
+      <span>Julián Andrés Gil</span>
+    </div>
+  </div>
+</div>
