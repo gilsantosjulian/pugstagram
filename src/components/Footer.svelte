d0cca15c0cef7ce2a787096f4518df28993ec108
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-05-05 06:52:54 -0500

    feat(Footer.svelte) new copy

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="Footer">
   <div class="Footer-container">
     <div class="Footer-copy">
-      <span>Copyright @ {new Date().getFullYear()} Pugstagram<span>
+      <span>Copyright @ {new Date().getFullYear()} Pugstagram from Platzi<span>
     </div> 
   </div>
 </div>
