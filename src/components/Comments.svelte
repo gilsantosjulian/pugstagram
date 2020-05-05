a8da2bf05f7ec07a6e29d1432a93c7db15198bae
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-05-05 09:37:20 -0500

    feat(Card/Commets/Timeline) add timeline, card and comments content

---
+++ src/components/Comments.svelte
@@ -0,0 +1,19 @@
+<div class="Comments">
+  <div class="Comments-content">
+    <div class="Comments-users">
+      <h3>Crispeta</h3>
+      <span>Hola Elmo</span>
+    </div>
+    <div class="Comments-add">
+      <form action="">
+        <input
+          id="text"
+          class='Comments-input' 
+          type="text"
+          placeholder="Agregar Comentario..."
+        >
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  </div>
+</div>
